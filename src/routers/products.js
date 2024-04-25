b06e36fd0b54220733262f92525df060fcdede19
Francia Mascarell <fran.mascarell@hotmail.com>
2024-04-25 00:33:08 -0400

    ok hasta producto

--- src/routers/products.js
+++ src/routers/products.js
@@ -15,4 +15,25 @@ router.get('/:pid', (req,res) => {
     return res.json({ producto: prod.getProductById(Number(pid))});
 });
 
+router.post('/',(req,res)=>{
+    const{title,description,price,thumbnails,code,stock,category,status}=req.body;
+    const prod = new ProductManager();
+    const resultado = prod.addProduct(title,description,price,thumbnails,code,stock,category,status);
+    return res.json({resultado});
+});
+
+router.put('/:pid',(req,res)=>{
+    const {pid}=req.params;
+    const prod = new ProductManager();
+    const resultado = prod.updateProduct(Number(pid),req.body);
+    return res.json({resultado});
+});
+
+router.delete('/:pid',(req,res)=>{
+    const {pid}=req.params;
+    const prod = new ProductManager();
+    const resultado = prod.deleteProduct(Number(pid));
+    return res.json({resultado});
+});
+
 export default router;
