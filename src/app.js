e4f43454bec967bf4630c4e811ef88b3b9647f91
Francia Mascarell <fran.mascarell@hotmail.com>
2024-04-29 11:51:39 -0400

    update

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
 import express from "express";
-import productsRouter from './routers/products.js'
-import cartsRouter from './routers/carts.js'
+import productsRouter from './routers/products.js';
+import cartsRouter from './routers/carts.js';
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.json);
+app.use(express.json()); //no olvidar los paréntesis de json()
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
-  return res.send('Bienvenidos a mi sitio');
-  console.log('Llega aca');
+app.get('/', (req, res) => {
+  console.log('Llega aca'); 
+  res.send('Bienvenidos a mi sitio');
 });
 
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`Run app in port ${PORT}`);
 });
